d4eb96f70c424698a3b80fdb6323b02cfc89afb4
ryota <ryota@Mac.local>
2019-12-13 11:57:24 +0900

    for designer

---
+++ components/dateBox.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <div :id="`box_${val2}`" class="dateBox">
+            <v-btn icon :id="`btn_${val2}`">
+            {{val1.getDate()}}
+            </v-btn>
+        </div>
+    </div>
+</template>
+<script >
+export default {
+    data(){
+        return{
+            today: new Date()
+        }
+    },
+    props:{
+        val1: Date,
+        val2: Number
+    },
+    mounted:function(){
+        const btn = document.getElementById('btn_'+this.val2)
+        const box = document.getElementById('box_'+this.val2)
+        if (this.val1.getDate()==this.today.getDate()){
+            box.style.border="2px solid black"
+            box.style.padding="1px 3.5px"
+        }
+        if(this.val1.getDay()===0){
+            btn.style.color="red"
+        }else if(this.val1.getDay()===6){
+            btn.style.color="blue"
+        }
+        if(this.val1.getMonth()<this.today.getMonth()&&this.val1<this.today){
+            box.style.backgroundColor="rgba(40,40,40,0.3)"
+        }else if(this.val1 < this.today && this.val1.getDate() != this.today.getDate()){
+            box.style.backgroundColor="grey"
+        }
+        if (this.val1.getMonth()!=this.today.getMonth()){
+            btn.style.opacity=0.4
+        }
+    },
+    methods:{
+    }
+}
+</script>
+<style >
+    .dateBox{
+        border: 1px solid rgb(0, 0, 0);
+        border-radius: 5px;
+        float:left;
+        width:150px;
+        height:150px;
+        padding: 2px 5px;
+        margin: 3px 3px;
+    }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,68 @@
+<template>
+    <div>
+        <div class="container">
+        <div class="showDays" id="calendar">
+            <div v-for="(count, index) in numberOfDays" :key="count" class="days">
+                <date-box class="oneDay" :val1=day(index) :val2=index />
+            </div>
+        </div>
+        <div class="backStr">{{month}}</div>
+        </div>
+    </div>
+</template>
+<script>
+import dateBox from '~/components/dateBox.vue'
+export default {
+    head:{
+        title: '出かけよう'
+    },
+    layout:'test',
+    data() {
+        return{
+            numberOfDays: 42,
+            today: new Date(),
+        }
+    },
+    computed: {
+        month(){
+            return (this.today.getMonth()+1)
+        }
+    },
+    components:{
+        dateBox
+    },
+    mounted: function() {
+        const cal = document.getElementById("calendar");
+        cal.scrollTop = 150;
+    },
+    methods:{
+        day(i){
+            const date = new Date()
+            date.setDate(this.today.getDate() -7 - this.today.getDay() + i);
+            return date
+        },
+    }
+}
+</script>
+<style scoped>
+.showDays{
+    height:1100px;
+    margin:50px auto;
+    overflow:auto;
+    background-color: rgba(255, 255, 255, 0.3);
+}
+.container{
+    width: 1200px;
+    position:relative;
+    background-color: rgba(255,255,255,0.3); 
+    z-index:3;
+}
+.backStr{
+    position: absolute;
+    top: 10%;
+    left:22%;
+    font-size: 380pt;
+    opacity: 0.2;
+    z-index:-2;
+}
+</style>
